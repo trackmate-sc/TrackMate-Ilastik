d7b25767cde74bd31a5a4454a974c0f980985b0f
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-05-29 21:27:57 +0200

    Update interactive test.

--- src/test/java/fiji/plugin/trackmate/TrackMateIlastikTestDrive.java
+++ src/test/java/fiji/plugin/trackmate/TrackMateIlastikTestDrive.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,7 +23,6 @@ package fiji.plugin.trackmate;
 
 import java.io.IOException;
 
-import net.imagej.Dataset;
 import net.imagej.ImageJ;
 
 public class TrackMateIlastikTestDrive
@@ -34,12 +33,13 @@ public class TrackMateIlastikTestDrive
 		final ImageJ ij = new ImageJ();
 		ij.launch( args );
 //		final String path = "D:\\Projects\\NVerttiQuintero\\Data\\Series014b.tif";
-		final String path = "D:\\Projects\\LLeBlanc\\Data\\201125_Proliferation_2C43-PilQmCherry-HupmRhubarb-II4_100X_timestep5min_Stage3_reg.tif";
+//		final String path = "D:\\Projects\\LLeBlanc\\Data\\201125_Proliferation_2C43-PilQmCherry-HupmRhubarb-II4_100X_timestep5min_Stage3_reg.tif";
+		final String path = "/Users/tinevez/Development/TrackMateWS/TrackMate-Ilastik/samples/NeisseriaMeningitidisGrowth.tif";
 //		"D:/Projects/NVerttiQuintero/Data/Series014b.tif"
-		final Dataset dataset = ( Dataset ) ij.io().open( path );
-		ij.ui().show( dataset );
+//		final Dataset dataset = ( Dataset ) ij.io().open( path );
+//		ij.ui().show( dataset );
 
-		new TrackMatePlugIn().run( null );
+		new TrackMatePlugIn().run( path );
 	}
 
 }
